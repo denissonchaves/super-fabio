1d155b23a050ce9dccfb2ec5a649427ab7635123
Denisson Chaves <denisson.chaves@hotmail.com>
2025-12-14 12:12:55 -0300

    add config files

--- js/Scene01.js
+++ js/Scene01.js
@@ -5,12 +5,30 @@ class Scene01 extends Phaser.Scene {
 
 	preload() {
 		this.load.image('background', 'assets/sprites/background-game.png');
+
 		this.load.image('ground', 'assets/sprites/chao.png');
-		this.load.image('platform', 'assets/sprites/plataforma.png');
-		this.load.image('brickPlatformSmall', 'assets/sprites/plataforma-tijolo-p.png');
-		this.load.image('brickPlatformMedium', 'assets/sprites/plataforma-tijolo-m.png');
-		this.load.image('brickPlatformLarge', 'assets/sprites/plataforma-tijolo-g.png');
+		this.load.image('platformSmall', 'assets/sprites/plataforma-p.png');
+		this.load.image('platformMedium', 'assets/sprites/plataforma-m.png');
+		this.load.image('platformLarge', 'assets/sprites/plataforma-g.png');
+
+		this.load.image('produto', 'assets/sprites/produto.png');
+		this.load.image('desenvolvimento', 'assets/sprites/desenvolvimento.png');
+		this.load.image('suporte', 'assets/sprites/suporte.png');
+		this.load.image('cloudInfra', 'assets/sprites/cloud-infra.png');
+		this.load.image('marketing', 'assets/sprites/marketing.png');
+		this.load.image('comercial', 'assets/sprites/comercial.png');
+		this.load.image('financeiro', 'assets/sprites/financeiro.png');
+		this.load.image('implantacao', 'assets/sprites/implantacao.png');
+		this.load.image('ecossistema', 'assets/sprites/ecossistema.png');
+		this.load.image('iopa', 'assets/sprites/iopa.png');
+		this.load.image('cs', 'assets/sprites/cs.png');
+		this.load.image('sdr', 'assets/sprites/sdr.png');
+
 		this.load.spritesheet('player', 'assets/sprites/player-parado.png', {
+			frameWidth: 105,
+			frameHeight: 198,
+		});
+		this.load.spritesheet('playerRunning', 'assets/sprites/player-correndo.png', {
 			frameWidth: 164,
 			frameHeight: 198,
 		});
@@ -19,14 +37,31 @@ class Scene01 extends Phaser.Scene {
 	create() {
 		this.background = this.add.image(0, 0, 'background').setOrigin(0, 0);
 
-		this.player = this.physics.add.sprite(50, 50, 'player');
-		this.player.setCollideWorldBounds(true).setScale(0.5);
+		this.player = this.physics.add
+			.sprite(50, 50, 'player')
+			.setCollideWorldBounds(true)
+			.setScale(0.5);
+		// .setBounce(0.2); // kickback when landing
 
 		this.player.canJump = true;
 
 		this.anims.create({
 			key: 'idle',
 			frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+			frameRate: 8,
+			repeat: -1,
+		});
+
+		this.anims.create({
+			key: 'run',
+			frames: this.anims.generateFrameNumbers('playerRunning', { start: 0, end: 8 }),
+			frameRate: 16,
+			repeat: -1,
+		});
+
+		this.anims.create({
+			key: 'jump',
+			frames: this.anims.generateFrameNumbers('playerRunning', { start: 3, end: 3 }),
 			frameRate: 8,
 			repeat: -1,
 		});
@@ -43,28 +78,81 @@ class Scene01 extends Phaser.Scene {
 		});
 
 		this.grounds = this.physics.add.staticGroup();
-		this.platforms = this.physics.add.staticGroup();
-		this.brickPlatformsSmall = this.physics.add.staticGroup();
-		this.brickPlatformsMedium = this.physics.add.staticGroup();
-		this.brickPlatformsLarge = this.physics.add.staticGroup();
+		this.platformsSmall = this.physics.add.staticGroup();
+		this.platformsMedium = this.physics.add.staticGroup();
+		this.platformsLarge = this.physics.add.staticGroup();
+
+		this.movingPlatform = this.physics.add
+			.image(200, 350, 'platformMedium')
+			.setImmovable(true)
+			.setVelocityX(100); // velocidade inicial
+		this.movingPlatform.body.allowGravity = false;
+
+		this.physics.add.collider(this.player, this.movingPlatform);
+
+		this.produto = this.physics.add.image(600, 100, 'produto');
 
 		this.grounds.create(0, 600, 'ground').setScale(2, 1).setOrigin(0, 1).refreshBody();
+		this.grounds.create(800, 600, 'ground').setScale(2, 1).setOrigin(0, 1).refreshBody();
 
-		this.brickPlatformsSmall.create(400, 450, 'brickPlatformSmall');
-		this.brickPlatformsSmall.create(700, 450, 'brickPlatformSmall');
-		this.brickPlatformsLarge.create(400, 300, 'brickPlatformLarge');
-		this.brickPlatformsLarge.create(600, 150, 'brickPlatformLarge');
+		this.platformsSmall.create(350, 450, 'platformSmall').refreshBody();
+		this.platformsSmall.create(700, 450, 'platformSmall').refreshBody();
+		this.platformsLarge.create(400, 300, 'platformLarge').refreshBody();
+		this.platformsLarge.create(600, 150, 'platformLarge').refreshBody();
 
 		this.physics.add.collider(this.player, this.grounds);
-		this.physics.add.collider(this.player, this.platforms);
-		this.physics.add.collider(this.player, this.brickPlatformsSmall);
-		this.physics.add.collider(this.player, this.brickPlatformsMedium);
-		this.physics.add.collider(this.player, this.brickPlatformsLarge);
+		this.physics.add.collider(this.player, this.platformsSmall);
+		this.physics.add.collider(this.player, this.platformsMedium);
+		this.physics.add.collider(this.player, this.platformsLarge);
+
+		this.physics.add.collider(this.produto, this.grounds);
+		this.physics.add.collider(this.produto, this.platformsSmall);
+		this.physics.add.collider(this.produto, this.platformsMedium);
+		this.physics.add.collider(this.produto, this.platformsLarge);
 
 		this.physics.world.bounds.width = this.background.width;
 		this.physics.world.bounds.height = this.background.height;
 		this.cameras.main.setBounds(0, 0, this.background.width, this.background.height);
 		this.cameras.main.startFollow(this.player);
+
+		// Textos para cada seta
+		const textosSetas = [
+			'Texto 1',
+			'Texto 2',
+			'Texto 3',
+			'Texto 4',
+			'Texto 5',
+			'Texto 6',
+			'Texto 7',
+			'Texto 8',
+			'Texto 9',
+			'Texto 10',
+			'Texto 11',
+			'Texto 12',
+		];
+
+		this.setas = this.physics.add.group();
+
+		for (let i = 0; i < 12; i++) {
+			// Crie a seta
+			const seta = this.setas.create(200 + i * 50, 200, 'seta');
+			seta.texto = textosSetas[i];
+
+			// Adicione o texto acima da seta
+			seta.label = this.add
+				.text(seta.x, seta.y - 40, seta.texto, {
+					font: '16px Arial',
+					fill: '#fff',
+					align: 'center',
+				})
+				.setOrigin(0.5);
+
+			// Opcional: mantenha referência para atualizar a posição do texto junto com a seta
+			seta.updateLabel = () => seta.label.setPosition(seta.x, seta.y - 40);
+		}
+
+		// Colisor entre player e setas
+		this.physics.add.overlap(this.player, this.setas, this.coletarSeta, null, this);
 	}
 
 	update() {
@@ -95,5 +183,29 @@ class Scene01 extends Phaser.Scene {
 		if (space) {
 			// Lógica para atirar
 		}
+
+		if (!this.player.body.touching.down) {
+			if (this.player.anims.currentAnim && this.player.anims.currentAnim.key !== 'jump') {
+				this.player.anims.play('jump');
+			}
+		} else if (left || right) {
+			if (this.player.anims.currentAnim && this.player.anims.currentAnim.key !== 'run') {
+				this.player.anims.play('run');
+			}
+		} else {
+			if (this.player.anims.currentAnim && this.player.anims.currentAnim.key !== 'idle') {
+				this.player.anims.play('idle');
+			}
+		}
+
+		if (this.movingPlatform.x >= 600) {
+			this.movingPlatform.setVelocityX(-100);
+		} else if (this.movingPlatform.x <= 200) {
+			this.movingPlatform.setVelocityX(100);
+		}
+        
+		this.setas.children.iterate((seta) => {
+			if (seta && seta.updateLabel) seta.updateLabel();
+		});
 	}
 }
